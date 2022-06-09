492e1495d40befb74aad47863c9cfb03d3331cd6
dianaprunaru <mosoianudiana@gmail.com>
2022-06-09 17:50:14 +0300

    Tema 31 mai_partea 2

--- M01S04/ex10/app.js
+++ M01S04/ex10/app.js
@@ -43,6 +43,53 @@ person.skills.forEach(function (skill, index) {
   }
 });
 
+console.warn(`In mod similar, afiseaza skillurile care contin litera a. `);
+
+var char = 'a';
+
+person.skills.forEach(function (skill) {
+  if (String.indexOf.char) {
+    console.log(skill);
+  }
+});
+
+console.warn(
+  ` Folosind forEach afiseaza propozitia: "Prietenii mei se numesc xxx yyy, xxx yyy si xxx yyy."`,
+);
+var message = '';
+var punctuation = ', ';
+person.friends.forEach(function (friend) {
+  if (i === person.friends.length - 1) {
+    punctuation = '.';
+  }
+  message +=
+    'Prietenii mei se numesc ' +
+    friend.name +
+    ' ' +
+    friend.surname +
+    punctuation;
+});
+console.log(message);
+// nu stiu ce gresesc aici. Oare var punctuation trebuie sa fie in interiorul forEach?
+
+console.warn(
+  `Folosind forEach, afiseaza numarul total de ani pe care il au
+  persoanele din arrayul friends, doar daca au varsta mai mare
+  decat 30 inclusiv.`,
+);
+person.friends.forEach(function (friend) {
+  // cum calculez suma varstelor tuturor prietenilor? Aceeasi lacuna am avut si la ex 06
+});
+console.log();
+
+console.warn(
+  `Folosind forEach, afiseaza suma anilor de nastere ai persoanelor care au varsta impara. `,
+);
+
+person.friends.forEach(function (friend) {
+  // same as previous
+});
+
 console.warn(
   `Afiseaza fraza: "Intre Dragos si Larry este o diferenta
   de xx ani. Intre Dragos si Steven... "`,
